46cbe2e135f08cd5800696d574ff3edf4b871cb8
Jakub Bańka <kubabanka@gmail.com>
2016-02-13 21:29:12 +0100

    Login

---
+++ TSST.Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TSST.Client
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            var fLogin = new LoginForm();
+            if (fLogin.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(Properties.Settings.Default.FacebookToken);
+                Application.Run(new MainForm());
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+    }
+}
